2cd4e061631b4c808b02cfcd53d6f23af17b9931
anchann <anchannnnnnnn@gmail.com>
2013-04-26 13:56:14 +0900

    The actual task

--- tasks/angular_verifybuild.js
+++ tasks/angular_verifybuild.js
@@ -9,42 +9,104 @@
 'use strict';
 
 module.exports = function(grunt) {
+	/**
+	 * @param filename relative to cwd
+	 * @return string or undefined
+	 */
+	var getRevvedFilename = function(filepath) {
+		var pieces   = filepath.split('/');
+		var filename = pieces.pop();
+		var path     = pieces.join('/');
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+		var filenameGlobPattern = path + '/*' + filename;
+		var revvedRegex = new RegExp('^' + path + '/[0-9a-fA-F]+\\.' + filename + '$');
 
-  grunt.registerMultiTask('angular_verifybuild', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+		var revved = grunt.util._.filter(grunt.file.expand(filenameGlobPattern), function(match) {
+			return revvedRegex.test(match);
+		});
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+		if (revved.length === 0) { return undefined; }
+		if (revved.length  >  1) { grunt.fail.warn('Multiple revved files found for ' + filepath); }
 
-      // Handle options.
-      src += options.punctuation;
+		return revved[0];
+	};
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+	grunt.registerTask('angular_verifybuild', 'Verify integrity of build artifacts for a yeoman angular project', function() {
+		var defaultOptions = {
+			exist: [
+				'index.html',
+			],
+			revved: [
+				'scripts/scripts.js',
+				'scripts/templates.js',
+				'styles/styles.css',
+			],
+			revvedRefs: {
+				'index.html': [
+					'scripts/scripts.js',
+					'scripts/templates.js',
+					'styles/styles.css',
+				],
+			},
+			dist: (grunt.config('yeoman') || {dist: ''}).dist
+		};
+
+		var options = this.options(defaultOptions);
+
+		grunt.util._.each(options.exist, function(filename) {
+			var fullFilename = options.dist + '/' + filename;
+
+			if (!grunt.file.exists(fullFilename)) {
+				grunt.log.error('Expected file does not exist: ' + fullFilename);
+			}
+			else {
+				grunt.log.ok('exists:    ' + fullFilename);
+			}
+		});
+
+		grunt.util._.each(options.revved, function(filename) {
+			var revvedFilename = getRevvedFilename(options.dist + '/' + filename);
+
+			if (revvedFilename === undefined) {
+				grunt.log.error('Expected revved file does not exist: ' + filename);
+			}
+			else {
+				grunt.log.ok('revved:    ' + filename + '      -->      ' + revvedFilename);
+			}
+		});
+
+		grunt.util._.each(options.revvedRefs, function(refs, filename) {
+			var revvedFilename = getRevvedFilename(options.dist + '/' + filename);
+
+			if (revvedFilename === undefined) {
+				// allow both revved and normal filenames
+				revvedFilename = options.dist + '/' + filename;
+				if (!grunt.file.exists(revvedFilename)) {
+					grunt.log.error('Expected file does not exist (either revved or plain): ' + filename);
+					return;
+				}
+			}
+
+			var content = grunt.file.read(revvedFilename);
+			grunt.util._.each(refs, function(ref) {
+				var revvedRefPath = getRevvedFilename(options.dist + '/' + ref);
+				if (revvedRefPath === undefined) {
+					grunt.log.error('revvedRef: no revved version of ' + ref + ' found.');
+					return;
+				}
+
+				var revvedRefFilename = revvedRefPath.split('/').pop();
+				if (new RegExp(revvedRefFilename.replace('.', '\\.')).test(content)) {
+					grunt.log.ok('revvedRef: ' + revvedFilename + '    -->     ' + revvedRefFilename);
+				}
+				else {
+					grunt.log.error('revvedRef: ' + revvedFilename + '     does not contain reference to     ' + revvedRefFilename);
+				}
+			});
+		});
+
+		return this.errorCount === 0;
+	});
 
 };
